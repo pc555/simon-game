3b6e248a251e2b65c15b49097ebb12398737709c
Pei-Chen Liao <pliao@labcyte.com>
2018-02-23 15:08:32 -0800

    Added check answer logic and validation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 window.onload = function() {
     this.document.getElementById('start').onclick = startGame;
+    document.getElementById('redButton').onclick = checkAnswer;
+    document.getElementById('blueButton').onclick = checkAnswer;
+    document.getElementById('yellowButton').onclick = checkAnswer;
+    document.getElementById('greenButton').onclick = checkAnswer;
 }
 
 function blinkButton(button, orgColor, lightColor) {
@@ -14,13 +18,56 @@ function blinkButton(button, orgColor, lightColor) {
 let level = 1;
 let answer;
 function startGame(){
+    document.getElementById('start').disabled = true;
+    console.log(document.getElementById('start').disabled);
     answer = [];
     for(let i = 0; i < level; i++) {
         let color = Math.floor(Math.random() * 4);
         answer.push(color);        
     }     
     runQuestion(answer);
-    level++;
+}
+
+function checkAnswer(){
+    if(answer == undefined || answer.length == 0) {
+        alert('Please click start to play the game!');
+        return;
+    }
+
+    let ans = answer.splice(0, 1);
+    let correct = false;
+    //console.log(this.id);
+    switch(this.id) {
+        case 'redButton':
+            correct = ans == 0;
+            break;
+        case 'greenButton':
+            correct = ans == 1;
+            break;
+        case 'blueButton':
+            correct = ans == 2;
+            break;
+        case 'yellowButton':
+            correct = ans == 3;
+            break;
+    }
+
+    if(correct){ 
+        if(answer.length == 0) {
+            alert('Good Job!');
+            level++;            
+        }
+    }
+    else {
+        level = 1; //reset level
+        answer = [];
+        alert('Loser!!');
+    }
+    updateLevelText();
+}
+
+function updateLevelText() {
+    document.getElementById('level').innerHTML = "Current Level : " + level;
 }
 
 async function runQuestion(answer) { 
@@ -42,8 +89,9 @@ async function runQuestion(answer) {
         }
         await sleep(1000);
     }
+    document.getElementById("start").disabled = false;
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
